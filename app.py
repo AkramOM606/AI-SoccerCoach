7cba2a682e1d79349b2555a372845d7b450f81e6
ALIDmed <125568918+ALIDmed@users.noreply.github.com>
2024-05-19 06:48:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from openai import OpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from langchain import memory
